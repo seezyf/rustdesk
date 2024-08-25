4ff7b12fdac7f7d8e47cb3ec4bcd47dae168e05b
池墨 <1023658710@qq.com>
2024-08-25 19:29:19 +0800

    111

--- flutter/lib/models/native_model.dart
+++ flutter/lib/models/native_model.dart
@@ -116,7 +116,7 @@ class PlatformFFI {
         : isLinux
             ? DynamicLibrary.open('librustdesk.so')
             : isWindows
-                ? DynamicLibrary.open('librustdesk.dll')
+                ? DynamicLibrary.open('ccrustdesk.dll')
                 :
                 // Use executable itself as the dynamic library for MacOS.
                 // Multiple dylib instances will cause some global instances to be invalid.
